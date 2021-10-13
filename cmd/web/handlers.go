ecc1c274139a4c720a1766e585000c93f9a06d44
petrostrak <pit.trak@gmail.com>
2021-10-13 13:21:04 +0300

    Template Composition

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -22,9 +22,17 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Initialize a slice containing the paths to the two files. Not that
+	// the home.page.tmpl file must be the *first* file in the slice.
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+	}
+
 	// Use the template.ParseFiles() function to read the template file
-	// into a template set.
-	ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+	// into a template set. Notice that we can pass the slice of files as
+	// a variadic parameter.
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
