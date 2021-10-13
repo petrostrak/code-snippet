4b1dd70982ab81ce051a07ee0da152a89d724d1a
petrostrak <pit.trak@gmail.com>
2021-10-13 13:43:23 +0300

    The http.FileServer Handler

--- cmd/web/url_mapping.go
+++ cmd/web/url_mapping.go
@@ -21,6 +21,19 @@ func StartApp() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	// Create a file server which serves files out of the ./ui/static/ dir.
+	// Note that the path given to the http.Dir() function is relative to the
+	// project directory root.
+	fs := http.FileServer(http.Dir("./ui/static/"))
+
+	// Use the mux.Handle() to register the file server as the handler for
+	// all URL paths that start with "/static". When the handler receives a
+	// request, it will remove the leading slash from the URL path and then
+	// search the ./ui/static directory for the corresponding file to send
+	// to the user. So, for this to work correctly, we must strip the leading
+	// "/static" from the URL path before passing it to http.FileServer.
+	mux.Handle("/static/", http.StripPrefix("/static", fs))
+
 	// Use the http.ListenAndServe() function to start a new web server. We pass
 	// two parameters: the TCP network address to listen on (in this case ":4000)
 	// and the servemux we just created. If http.ListenAndServe() returns an error
