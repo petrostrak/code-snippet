31b04f50974b23fd555c18ceba540b55df4c0cfd
petrostrak <pit.trak@gmail.com>
2021-10-14 20:51:11 +0300

    Executing SQL Statements

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -6,6 +6,10 @@ import (
 	"github.com/petrostrak/code-snippet/pkg/models"
 )
 
+// DB.Query() is used for SELECT queries which return multiple rows.
+// DB.QueryRow() is used for SELECT queries which return a single row.
+// DB.Exec() is used for statements which don’t return rows (like INSERT and DELETE).
+
 // Define a SnippetModel type which wraps a sql.DB connection pool.
 type SnippetModel struct {
 	DB *sql.DB
@@ -13,7 +17,24 @@ type SnippetModel struct {
 
 // This will insert a new snippet into the database.
 func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-	return 0, nil
+
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+			 VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	// Use the Exec() method on the embedded connection pool to execute the statement.
+	// This method returns a sql.Result object, which contains some basic information
+	// about what happend when the statement was executed.
+	rs, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := rs.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }
 
 // This will return a specific snippet based on its id.
