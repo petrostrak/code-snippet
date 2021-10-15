a44938e9b16d4fde30ca465c73f51791fe478398
petrostrak <pit.trak@gmail.com>
2021-10-15 15:47:47 +0300

    Render helper function for duplicate code in home and showSnippet

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -28,3 +28,20 @@ func (a *application) clientError(w http.ResponseWriter, status int) {
 func (a *application) notFound(w http.ResponseWriter) {
 	a.clientError(w, http.StatusNotFound)
 }
+
+func (a *application) render(w http.ResponseWriter, r *http.Request, name string, td *templateData) {
+
+	//Retrive the appropriate template set from the cache based on the page name
+	// (like 'home.page.tmpl'). If no entry exists in the cache with the provided
+	// name, we call the serverError.
+	ts, ok := a.templateCache[name]
+	if !ok {
+		a.serverError(w, fmt.Errorf("the template %s does not exist", name))
+		return
+	}
+
+	// Execute the template set, passing in any dynamic data.
+	if err := ts.Execute(w, td); err != nil {
+		a.serverError(w, err)
+	}
+}
