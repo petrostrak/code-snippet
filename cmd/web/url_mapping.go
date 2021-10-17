a813142a2386db511320b0195e161fa2919106fc
petrostrak <pit.trak@gmail.com>
2021-10-17 23:49:05 +0300

    Configuring HTTPS Settings

--- cmd/web/url_mapping.go
+++ cmd/web/url_mapping.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"database/sql"
 	"flag"
 	"html/template"
@@ -83,13 +84,24 @@ func StartApp() {
 		templateCache: templateCache,
 	}
 
+	// Initialize a tls.Config struct to hold the non-default TLS settings we
+	// will set the server to use.
+	tlsConfig := &tls.Config{
+		PreferServerCipherSuites: true,
+		CurvePreferences:         []tls.CurveID{tls.X25519, tls.CurveP256},
+	}
+
 	// Initialize a new http.Server struct. We set the Addr and Handler fields
 	// that the server uses the same network address and routes as before, and
 	// the ErrorLog field so that the server now uses the custom errorLog logger.
+	//
+	// Set the server's TLSConfig field to use the tlsConfig variable we just
+	// created.
 	svr := &http.Server{
-		Addr:     *addr,
-		ErrorLog: errorLog,
-		Handler:  app.routes(),
+		Addr:      *addr,
+		ErrorLog:  errorLog,
+		Handler:   app.routes(),
+		TLSConfig: tlsConfig,
 	}
 
 	infoLog.Printf("Starting server on %s\n", *addr)
