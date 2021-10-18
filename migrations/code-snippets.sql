082c414e00acf0e7fa19fe092a063caa1739586c
petrostrak <pit.trak@gmail.com>
2021-10-18 10:35:58 +0300

    Creating a Users Model - sql

--- migrations/code-snippets.sql
+++ migrations/code-snippets.sql
@@ -43,3 +43,13 @@ CREATE USER 'web'@'localhost';
 GRANT SELECT, INSERT ON codesnippet.* TO 'web'@'localhost';
 -- Important: Make sure to swap 'pass' with a password of your own choosing.
 ALTER USER 'web'@'localhost' IDENTIFIED BY 'pass';
+
+CREATE TABLE users (
+id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL,
+hashed_password CHAR(60) NOT NULL,
+created DATETIME NOT NULL
+);
+
+ALTER TABLE users ADD CONSTRAINT users_uc_email UNIQUE (email);
