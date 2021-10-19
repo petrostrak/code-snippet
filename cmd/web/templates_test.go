d6a4981416e4e844b933826dff5049ba84a01baf
petrostrak <pit.trak@gmail.com>
2021-10-19 16:59:25 +0300

    Creating a Unit Test

---
+++ cmd/web/templates_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+// go test -v ./cmd/web
+func TestHumanDate(t *testing.T) {
+	// Initialize a new time.Time object and pass it to the humanDate function.
+	tm := time.Date(2020, 12, 17, 10, 0, 0, 0, time.UTC)
+	hd := humanDate(tm)
+
+	// Check that the output from the humanDate function is in the format we
+	// expext. If it isn't what we expect, use the t.Errorf() to indicate that
+	// the test has failed and log the expected and actual values.
+	if hd != "17 Dec 2020 at 10:00" {
+		t.Errorf("want %q; got %q", "17 Dec 2020 at 10:00", hd)
+	}
+}
